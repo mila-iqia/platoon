69730e8fea255d21a1539020b96419185b76f1ea
Mathieu Germain <mathieu.germain@gmail.com>
2016-01-18 17:13:34 -0500

    Modified the LSTM exampel so it's compatible with the launcher.

--- example/lstm/imdb.py
+++ example/lstm/imdb.py
@@ -61,6 +61,7 @@ def get_dataset_file(dataset, default_dataset, origin):
         new_path = os.path.join(
             os.path.split(__file__)[0],
             "..",
+            "..",
             "data",
             dataset
         )
