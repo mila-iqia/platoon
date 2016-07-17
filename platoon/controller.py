ab44c7ce7bd31f24600289dfdc4b305bda1c8405
Christos Tsirigotis <tsirif@gmail.com>
2016-07-17 17:56:28 +0300

    Add mechanism to inform a worker about gpucomm regional info

---
+++ platoon/controller.py
@@ -0,0 +1,210 @@
+import os
+
+import numpy
+import posix_ipc
+import zmq
+from mpi4py import MPI
+
+
+class Controller(object):
+    """
+    Abstract multi-process controller
+
+    This class provides the necessary features to dispatch data mini-batches
+    to workers and handle control requests. Using this class should be done
+    by having another class inherit from it and override the method
+    `handle_control()`.
+
+    .. warning::
+
+        Due to the underlying implementation it is a bad idea to
+        attempt to do both in the same process, even on different
+        threads.  This will suffer from interlock problems and may
+        negate any speedup you could get from using multiple Workers.
+
+        Because of this issue, the class may be split in the future.
+
+    Parameters
+    ----------
+    port : int
+        The port number to communicate over
+    control_port : int
+        The control port number.
+    device_list : list of strings
+        Contains device names in clique order (prefer ring topology)
+    hwm : int
+        High water mark (see pyzmq docs).
+
+    """
+
+    def __init__(self, control_port, device_list, global_rank,
+                 port=None, hwm=10, global_comm_id=""):
+
+        self._should_stop = False
+        self._worker_list = set()
+        self._need_init = True
+
+        self._device_list = device_list
+        # Controllers' MPI comm info
+        self._global_rank = global_rank
+        self._global_comm_id = "platoon-global_comm_" + global_comm_id
+        self._global_comm = None
+        # These 3 variables below will be used to take advantage of multi-node
+        # support of NCCL (later)
+        self._local_size = 0
+        self._controller_rank = 0
+        self._region_size = 0
+
+        if port:
+            self.init_data(port, hwm)
+
+        self._init_control_socket(control_port)
+
+        # Cleanup and init global lock and job_uid name ##
+        self._job_uid = "platoon_{0}_{1}".format(
+            os.path.basename(os.path.expanduser('~')), control_port)
+
+        self._lock_name = "{}lock".format(self._job_uid)
+        # The ExistentialError is apparently the only way to verify if
+        # the semaphore/shared_memory exists.
+        try:
+            posix_ipc.unlink_semaphore(self._lock_name)
+        except posix_ipc.ExistentialError:
+            pass
+        # Initializing lock
+        posix_ipc.Semaphore(self._lock_name, posix_ipc.O_CREAT,
+                            initial_value=1)
+
+    def init_data(self, port, hwm=10):
+        """
+        Initialize the mini-batch socket.
+
+        This must be called before using :meth:`send_mb`.
+
+        Parameters
+        ----------
+        port : int
+            The port to listen on.
+        hwm : int
+            High water mark, see the pyzmq docs.
+
+        """
+        acontext = zmq.Context()
+        self.asocket = acontext.socket(zmq.PUSH)
+        self.asocket.set_hwm(hwm)
+        self.asocket.bind('tcp://*:{}'.format(port))
+
+    def _init_control_socket(self, port):
+        """
+        Initialize the control socket.
+
+        This must be called before using :meth:`serve`.
+
+        Parameters
+        ----------
+        port : int
+            The port to listen on.
+
+        """
+        ccontext = zmq.Context()
+        self.csocket = ccontext.socket(zmq.REP)
+        self.csocket.bind('tcp://*:{}'.format(port))
+
+    def send_mb(self, arrays):
+        """
+        Send a mini-batch over the socket.
+
+        This function may block if arrays are being sent faster than
+        the clients can handle.
+
+        Parameters
+        ----------
+        arrays : list of ndarrays
+            List of numpy.ndarray to send.  All arrays should be
+            contiguous for better performance.
+
+        """
+        # The buffer protocol only works on contiguous arrays
+        arrays = [numpy.ascontiguousarray(array) for array in arrays]
+        headers = [numpy.lib.format.header_data_from_array_1_0(array)
+                   for array in arrays]
+        self.asocket.send_json(headers, zmq.SNDMORE)
+        for array in arrays[:-1]:
+            self.asocket.send(array, zmq.SNDMORE)
+        self.asocket.send(arrays[-1])
+
+    def handle_control(self, req, worker_id, req_info):
+        """
+        Re-implement or assign a handler to this function to do
+        something with control messages.
+
+        The replacement get one parameter which is the request and
+        should return the response which must be a json-encodable
+        object. Other code is responsible for handling decoding,
+        encoding and the network.
+
+        """
+        raise NotImplementedError("The Controller class should not be "
+                                  "instantiated directly. Classes that "
+                                  "inherit from Controller should override "
+                                  "the method `handle_control()`")
+
+    def _handle_base_control(self, req, worker_id, req_info):
+        """
+        This method handle base control commands.
+        Those commands should not be used in the handle_control method.
+        All base control commands should start with "platoon-".
+        """
+        response = None
+        if req == "platoon-get_job_uid":
+            self._local_size += 1
+            response = self._job_uid
+
+        elif req == "platoon-get_device":
+            response = self._device_list[req_info['local_rank']]
+
+        elif req == "platoon-need_init":
+            response = self._need_init
+            self._need_init = False
+
+        elif req == "platoon-get_region_info":
+            # Init MPI and decide for a Platoon region on which Controller rules
+            if self._global_comm is None:
+                self._region_id = b"platoon-" + req_info['region_id']
+                self._region_size = self._local_size
+                self._init_global_comm()
+            response = dict()
+            response['region_id'] = self._region_id
+            response['region_size'] = self._region_size
+            response['regional_rank'] = self._controller_rank + req_info['local_rank']
+
+        return response
+
+    def worker_is_done(self, worker_id):
+        self._worker_list.discard(worker_id)
+        self._should_stop = True
+
+    def serve(self):
+        """
+        This method will handle control messages until the should_stop flag
+        has been raised and that all the known worker are done.
+        """
+
+        while (not self._should_stop) or self._worker_list:
+            query = self.csocket.recv_json()
+            self._worker_list.add(query['worker_id'])
+
+            response = self._handle_base_control(query['req'],
+                                                 query['worker_id'],
+                                                 query['req_info'])
+            if response is None:
+                response = self.handle_control(query['req'],
+                                               query['worker_id'],
+                                               query['req_info'])
+
+            self.csocket.send_json(response)
+        self.csocket.close()
+
+    def _init_global_comm(self):
+        # TODO
+        pass
--- platoon/worker.py
+++ platoon/worker.py
@@ -5,6 +5,7 @@ import numpy
 import posix_ipc
 import six
 import zmq
+from pygpu import collectives as gpucoll
 
 # You need:
 # $ conda install pyzmq cffi
@@ -16,175 +17,6 @@ if six.PY3:
     buffer_ = memoryview
 else:
     buffer_ = buffer  # noqa
-
-
-class Controller(object):
-    """
-    Abstract multi-process controller
-
-    This class provides the necessary features to dispatch data mini-batches
-    to workers and handle control requests. Using this class should be done
-    by having another class inherit from it and override the method
-    `handle_control()`.
-
-    .. warning::
-
-        Due to the underlying implementation it is a bad idea to
-        attempt to do both in the same process, even on different
-        threads.  This will suffer from interlock problems and may
-        negate any speedup you could get from using multiple Workers.
-
-        Because of this issue, the class may be split in the future.
-
-    Parameters
-    ----------
-    port : int
-        The port number to communicate over
-    control_port : int
-        The control port number.
-    hwm : int
-        High water mark (see pyzmq docs).
-
-    """
-
-    def __init__(self, control_port, port=None, hwm=10):
-
-        self._should_stop = False
-        self._worker_list = set()
-        self._need_init = True
-
-        if port:
-            self.init_data(port, hwm)
-
-        self._init_control_socket(control_port)
-
-        # Cleanup and init global lock and job_uid name ##
-        self._job_uid = "platoon_{0}_{1}".format(
-                os.path.basename(os.path.expanduser('~')), control_port)
-
-        self._lock_name = "{}lock".format(self._job_uid)
-        # The ExistentialError is apparently the only way to verify if
-        # the semaphore/shared_memory exists.
-        try:
-            posix_ipc.unlink_semaphore(self._lock_name)
-        except posix_ipc.ExistentialError:
-            pass
-        # Initializing lock
-        posix_ipc.Semaphore(self._lock_name, posix_ipc.O_CREAT,
-                            initial_value=1)
-
-    def init_data(self, port, hwm=10):
-        """
-        Initialize the mini-batch socket.
-
-        This must be called before using :meth:`send_mb`.
-
-        Parameters
-        ----------
-        port : int
-            The port to listen on.
-        hwm : int
-            High water mark, see the pyzmq docs.
-
-        """
-        acontext = zmq.Context()
-        self.asocket = acontext.socket(zmq.PUSH)
-        self.asocket.set_hwm(hwm)
-        self.asocket.bind('tcp://*:{}'.format(port))
-
-    def _init_control_socket(self, port):
-        """
-        Initialize the control socket.
-
-        This must be called before using :meth:`serve`.
-
-        Parameters
-        ----------
-        port : int
-            The port to listen on.
-
-        """
-        ccontext = zmq.Context()
-        self.csocket = ccontext.socket(zmq.REP)
-        self.csocket.bind('tcp://*:{}'.format(port))
-
-    def send_mb(self, arrays):
-        """
-        Send a mini-batch over the socket.
-
-        This function may block if arrays are being sent faster than
-        the clients can handle.
-
-        Parameters
-        ----------
-        arrays : list of ndarrays
-            List of numpy.ndarray to send.  All arrays should be
-            contiguous for better performance.
-
-        """
-        # The buffer protocol only works on contiguous arrays
-        arrays = [numpy.ascontiguousarray(array) for array in arrays]
-        headers = [numpy.lib.format.header_data_from_array_1_0(array)
-                   for array in arrays]
-        self.asocket.send_json(headers, zmq.SNDMORE)
-        for array in arrays[:-1]:
-            self.asocket.send(array, zmq.SNDMORE)
-        self.asocket.send(arrays[-1])
-
-    def handle_control(self, req, worker_id):
-        """
-        Re-implement or assign a handler to this function to do
-        something with control messages.
-
-        The replacement get one parameter which is the request and
-        should return the response which must be a json-encodable
-        object. Other code is responsible for handling decoding,
-        encoding and the network.
-
-        """
-        raise NotImplementedError("The Controller class should not be "
-                                  "instantiated directly. Classes that "
-                                  "inherit from Controller should override "
-                                  "the method `handle_control()`")
-
-    def _handle_base_control(self, req, worker_id):
-        """
-        This method handle base control commands.
-        Those commands should not be used in the handle_control method.
-        All base control commands should start with "platoon-".
-        """
-        response = None
-        if req == "platoon-get_job_uid":
-            response = self._job_uid
-
-        elif req == "platoon-need_init":
-            response = self._need_init
-            self._need_init = False
-
-        return response
-
-    def worker_is_done(self, worker_id):
-        self._worker_list.discard(worker_id)
-        self._should_stop = True
-
-    def serve(self):
-        """
-        This method will handle control messages until the should_stop flag
-        has been raised and that all the known worker are done.
-        """
-
-        while (not self._should_stop) or self._worker_list:
-            query = self.csocket.recv_json()
-            self._worker_list.add(query['worker_id'])
-
-            response = self._handle_base_control(query['req'],
-                                                 query['worker_id'])
-            if response is None:
-                response = self.handle_control(query['req'],
-                                               query['worker_id'])
-
-            self.csocket.send_json(response)
-        self.csocket.close()
 
 
 class Worker(object):
@@ -215,12 +47,13 @@ class Worker(object):
 
     """
 
-    def __init__(self, control_port, port=None, socket_timeout=300000, hwm=10):
+    def __init__(self, control_port, local_rank, port=None, socket_timeout=300000, hwm=10):
         self.context = zmq.Context()
 
         self._socket_timeout = socket_timeout
 
         self._worker_id = os.getpid()
+        self._local_rank = local_rank
 
         if port:
             self.init_mb_sock(port, hwm)
@@ -228,7 +61,27 @@ class Worker(object):
         self._init_control_socket(control_port)
 
         self._job_uid = self.send_req("platoon-get_job_uid")
+        self._device_name = self.send_req("platoon-get_device",
+                                          info={'local_rank': self._local_rank})
         self._lock = posix_ipc.Semaphore("{}lock".format(self._job_uid))
+
+        self._register_to_platoon()
+
+    def _register_to_platoon(self):
+        # Set Theano device to be used by Theano
+        # Get pygpu context used by Theano
+        gpuctx = None
+        self._region_id = gpucoll.GpuCommCliqueId(context=gpuctx)
+        # Ask controller for region's info to participate in
+        response = self.send_req("platoon-get_region_info",
+                                 info={'local_rank': self._local_rank,
+                                       'region_id': self._region_id.comm_id})
+        self._region_id.comm_id = bytearray(response['region_id'])
+        self._regional_comm = gpucoll.GpuComm(self._region_id,
+                                              response['region_size'],
+                                              response['regional_rank'])
+
+        pass
 
     def init_mb_sock(self, port, hwm=10):
         """
@@ -477,7 +330,7 @@ class Worker(object):
         if synchronous:
             self.unlock_params()
 
-    def send_req(self, req):
+    def send_req(self, req, info=None):
         """
         Send a control request.
 
@@ -486,6 +339,7 @@ class Worker(object):
         req : object
             This is a json-encodable object that will be sent to the
             Controller.
+        info : object
 
         Returns
         -------
@@ -493,7 +347,7 @@ class Worker(object):
             Json-decoded object
 
         """
-        query = {"worker_id": self._worker_id, "req": req}
+        query = {'worker_id': self._worker_id, 'req': req, 'req_info': info}
         self.csocket.send_json(query)
 
         socks = dict(self.cpoller.poll(self._socket_timeout))
