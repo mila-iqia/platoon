bced383e7071a7de3b1649aa061d769eac27ee00
Christos Tsirigotis <tsirif@gmail.com>
2016-08-09 22:45:17 +0300

    Add channel.py to ensure backwards compatibility

---
+++ platoon/channel.py
@@ -0,0 +1,8 @@
+"""
+File: channel.py
+Description: This file serves as a backwards compatibility layer for Platoon v0.5.0
+"""
+
+from __future__ import absolute_import
+from .worker import Worker
+from .controller import Controller
