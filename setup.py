7deddba05d910205e98684d17fc9f41be2b24877
Christos Tsirigotis <tsirif@gmail.com>
2016-08-23 23:53:23 +0300

    Bump up Platoon version to 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='platoon',
-    version='0.5.0',
+    version='0.6.0',
     author='MILA',
     packages=['platoon'],
     scripts=['scripts/platoon-launcher'],
